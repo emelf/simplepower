c240c69b884384a76a553be224a20a17d407c270
Emil Melfald <emelf@usn.no>
2023-11-23 21:19:46 +0100

    Updates to the generator model

--- gen_model_test.py
+++ gen_model_test.py
@@ -36,13 +36,12 @@ class GenModelThirdOrder:
         """Initializes the dynamic model from the power flow solution."""
         # This init function must find the following quantities: 
         
-        # [delta, D_omega, E_q_t, P_e, I_d, I_q, X_d, X_q, k_d, k_q, E_f, P_m]
+        # [delta, D_omega, P_m, E_f, E_q_t, P_e, I_d, I_q]
         I_a = np.sqrt(P_g_pu**2 + Q_g_pu**2)/V_g_pu
         phi = np.arctan(Q_g_pu/P_g_pu)
         I_d_init = -I_a*np.sin(phi)
         I_q_init = I_a*np.cos(phi)
-        x0 = np.array([0, 0, delta_t, P_g_pu, I_d_init, I_q_init, self.X_d_u, self.X_q_u, 
-                       1.0, 1.0, delta_t, P_g_pu])
+        x0 = np.array([0, 0, P_g_pu, V_g_pu, V_g_pu, P_g_pu, I_d_init, I_q_init])
         sol = root(self._init_from_pf_objective, x0, args=(P_g_pu, Q_g_pu, delta_t, V_g_pu))
         delta, D_omega, E_q_t, P_e, I_d, I_q, X_d, X_q, k_d, k_q, E_f, P_m = sol.x
         X0 = np.array([delta, D_omega, E_q_t])
@@ -64,25 +63,30 @@ class GenModelThirdOrder:
         dE_q_t_dt = (E_f - E_q_t + I_d*(self.data.X_d_u - self.data.X_d_u_t))/self.data.T_do_t
         return np.array([ddelta_dt, dD_omega_dt, dE_q_t_dt], dtype=np.float64)
 
-    def _T_f(self, X):
-        delta, _, _, _ = X 
+    def _T_f(self, delta):
         return np.array([[-np.sin(delta), np.cos(delta)], [np.cos(delta), np.sin(delta)]])
     
     def _init_from_pf_objective(self, x, P_g, Q_g, V_t, delta_t): 
-        delta, D_omega, E_q_t, P_e, I_d, I_q, X_d, X_q, k_d, k_q, E_f, P_m = x
+        delta, D_omega, P_m, E_f, E_q_t, P_e, I_d, I_q = x
         V_a, V_b = np.array([V_t*np.cos(delta_t), V_t*np.sin(delta_t)])
         V_dq = self._T_f(delta) @ np.array([V_a, V_b])
 
         # First assume the state diff equations are 0 
         f1 = D_omega 
         f2 = P_m - P_e - self.data.D*D_omega
-        f3 = E_f - E_q_t + I_d*(X_d - self.X_d_t)
+        f3 = E_f - E_q_t + I_d*(self.data.X_d_u - self.data.X_d_u_t)
 
         # Then find the root of the algebraic equations
-        f4 = P_e - (E_q_t*I_q + (self.data.X_d_u_t - self.data.X_q_u)*I_d*I_q)
-        f5 = P_g - (V_dq[0]*I_d + V_dq[1]*I_q)
-        f6 = Q_g - (-V_dq[1]*I_d + V_dq[0]*I_q)
-        f7, f8 = np.array([I_d, I_q]) - (self.data._Y@(np.array([0, E_q_t]) - V_dq))
+        P_e_, I_d_, I_q_, P_g_, Q_g_ = self.get_alg_vars(x, np.array([E_f, P_m]))
+        f4 = P_e - P_e_
+        f5 = P_g - P_g_
+        f6 = Q_g - Q_g_
+        f7 = I_d - I_d_ 
+        f8 = I_q - I_q_ 
+        # f4 = P_e - (E_q_t*I_q + (self.data.X_d_u_t - self.data.X_q_u)*I_d*I_q)
+        # f5 = P_g - (V_dq[0]*I_d + V_dq[1]*I_q)
+        # f6 = Q_g - (-V_dq[1]*I_d + V_dq[0]*I_q)
+        # f7, f8 = np.array([I_d, I_q]) - (self.data._Y@(np.array([0, E_q_t]) - V_dq))
 
         return np.array([f1,f2,f3,f4,f5,f6,f7,f8])
     
@@ -91,12 +95,10 @@ class GenModelThirdOrder:
         E_f, P_m, V_t, delta_t = u
         V_a, V_b = np.array([V_t*np.cos(delta_t), V_t*np.sin(delta_t)])
         V_d, V_q = self._T_f(delta) @ np.array([V_a, V_b])
-        I_d, I_q = self.Y @ (np.array([0, E_q_t]) - np.array([V_d, V_q]))
-
+        I_d, I_q = self.data._Y @ (np.array([0, E_q_t]) - np.array([V_d, V_q]))
         P_g = V_d*I_d + V_q*I_q
         Q_g = -V_q*I_d + V_d*I_q
         P_e = E_q_t*I_q + (self.data.X_d_u_t - self.data.X_q_u)*I_d*I_q
-
         return np.array([P_e, I_d, I_q, P_g, Q_g])
         
     
